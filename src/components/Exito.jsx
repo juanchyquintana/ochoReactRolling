ff2f68d132f49d5ba51e4cd07a9d2500cb69d5b6
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-01-16 20:03:12 -0300

    se creó la funcion para validar los campos y tambien se crearon los componentes de alerta para el error y el envió correcto de los datos

---
+++ src/components/Exito.jsx
@@ -0,0 +1,7 @@
+const Exito = ({children}) => {
+    return (
+      <div className="my-3 text-center text-uppercase text-white fw-bold bg-success rounded-5 py-1">{children}</div>
+    )
+  }
+  
+  export default Exito
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import Error from "./Error";
+import Exito from "./Exito";
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [dni, setDni] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [error, setError] = useState(false);
+  const [exito, setExito] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Se validan los campos
+    if ([nombre, apellido, dni, email].includes("")) {
+      setError(true);
+      return;
+    }
+    setError(false);
+
+    // Limpiar Formulario
+    setNombre("");
+    setApellido("");
+    setDni("");
+    setEmail("");
+
+    setExito(true);
+
+    setTimeout(() => {
+      setExito(false);
+    }, 3000);
+  };
+
   return (
     <>
       <section className="mt-3 bg-white mt-5 p-4 rounded shadow">
-        <Form>
+        <Form onSubmit={handleSubmit}>
+
+          {error && <Error>Hay campos vácios</Error>}
+          {exito && <Exito>Datos enviados correctamente</Exito>}
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Nombre</Form.Label>
             <Form.Control
@@ -12,6 +50,8 @@ const Formulario = () => {
               placeholder="Introduzca sus nombres"
               minLength={3}
               maxLength={100}
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
             />
           </Form.Group>
 
@@ -22,6 +62,8 @@ const Formulario = () => {
               placeholder="Introduzca sus apellidos"
               minLength={3}
               maxLength={100}
+              value={apellido}
+              onChange={(e) => setApellido(e.target.value)}
             />
           </Form.Group>
 
@@ -31,6 +73,8 @@ const Formulario = () => {
               type="number"
               placeholder="Introduzca su DNI"
               maxLength={8}
+              value={dni}
+              onChange={(e) => setDni(e.target.value)}
             />
           </Form.Group>
 
@@ -40,17 +84,15 @@ const Formulario = () => {
               type="email"
               placeholder="correo@correo.com"
               maxLength={60}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
 
           <Button
             variant="danger"
             type="submit"
-            className="text-uppercase fw-bold w-100"
+            className="text-uppercase fw-bold w-100 mt-3"
           >
             Enviar
           </Button>
